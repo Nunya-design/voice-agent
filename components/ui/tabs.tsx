47d531668d27cc4d4a89acbb8617e378a141408b
Nunya-design <waltlafky@gmail.com>
2025-04-04 19:01:38 -0700

    Create tabs.tsx

---
+++ components/ui/tabs.tsx
@@ -0,0 +1,39 @@
+npm install @radix-ui/react-tabs
+"use client";
+
+import * as TabsPrimitive from "@radix-ui/react-tabs";
+import { cn } from "@/lib/utils";
+import * as React from "react";
+
+const Tabs = TabsPrimitive.Root;
+
+const TabsList = React.forwardRef<
+  React.ElementRef<typeof TabsPrimitive.List>,
+  React.ComponentPropsWithoutRef<typeof TabsPrimitive.List>
+>(({ className, ...props }, ref) => (
+  <TabsPrimitive.List
+    ref={ref}
+    className={cn("inline-flex items-center gap-2 p-1 bg-white rounded-2xl shadow-sm", className)}
+    {...props}
+  />
+));
+TabsList.displayName = "TabsList";
+
+const TabsTrigger = React.forwardRef<
+  React.ElementRef<typeof TabsPrimitive.Trigger>,
+  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Trigger>
+>(({ className, ...props }, ref) => (
+  <TabsPrimitive.Trigger
+    ref={ref}
+    className={cn(
+      "inline-flex min-w-[80px] items-center justify-center rounded-xl px-3 py-1 text-sm font-medium text-gray-700 hover:bg-gray-100 data-[state=active]:bg-black data-[state=active]:text-white",
+      className
+    )}
+    {...props}
+  />
+));
+TabsTrigger.displayName = "TabsTrigger";
+
+const TabsContent = TabsPrimitive.Content;
+
+export { Tabs, TabsList, TabsTrigger, TabsContent };
